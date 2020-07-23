041fdae652b59a4c4fcdd9697848f793455478d7
Fleshborne <blakeathompson94@gmail.com>
2020-07-23 18:08:10 -0400

    got server to work, mongodb is a pain, keeps shutting off

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.use(express.static('./Develop/public'));
+
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+// set up routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
