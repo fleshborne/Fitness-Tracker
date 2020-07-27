83e34b20bccad1954f5cbf06214b94178375d425
Fleshborne <blakeathompson94@gmail.com>
2020-07-26 20:33:56 -0400

    routes working now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,47 @@
 const router = require('express').Router();
-const Workout = require('../models/workout');
+const db = require('../models/');
+// CRUD implementation 
 
-router.post('/api/workout', (req, res) => {
-    Workout.Create({}).then();
+// CREATE
+router.post('/workouts', (req, res) => {
+    db.Workout.create({})
+        .then(workout => res.json(workout))
+        .catch(err => res.json(err));
 });
+
+router.post('/workouts', (req, res) => {
+    db.Workout.create(req.body)
+        .then((workout) => res.json(workout))
+        .catch((err) => res.json(err));
+})
+
+// READ
+router.get('/workouts', (req, res) => {
+    db.Workout.find()
+        .then((workout) => res.json(workout))
+        .catch((err) => (err));
+});
+router.get('/workouts/range', (req, res) => {
+    db.Workout.find()
+        .then((workout) => res.json(workout))
+        .catch((err) => res.json(err));
+})
+
+// UPDATE
+router.put('/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            },
+        }).then((workout) => res.json(workout))
+        .catch((err) => res.json(err));
+
+});
+
+// router.put('')
+
+// TODO: create delete workout button and call new route.
+// router.put('/workouts/:id, (req,res) => {
+//  db.Workout.findByIdAndDelete({}).then();
+// })
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,16 @@
 const router = require('express').Router();
 const path = require('path');
-
+// html routes for each page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
-    // res.send("Home Page");
 });
 
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../Develop/public/stats.html'));
-    // res.send("Stats Page");
 });
 
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../Develop/public/exercise.html'));
-    // res.send("Exercise Page");
 });
 
 module.exports = router;
